d8659a15d8eb2c3578ef08ac83aefccc8a4eb491
Yuri Miguel <yuri.md13@gmail.com>
2023-12-19 17:01:03 -0300

    declaração de serviço main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
 provider "aws" {
-    region = "us-east-1"
+    region = "us-east-1" #Região desejada
 }
+
+resource "aws_instance" "web_server" {
+    ami           = "ami-0c55b159cbfafe1f0" #AMI do Amazon Linux 2 (pode substituir de acordo com a necessidade)
+    instance_type = "t2.micro" #Tipo da máquina, neste caso uma de baixo custo/performance (também pode ser modificada de acordo com a necessidade)
+}
+
