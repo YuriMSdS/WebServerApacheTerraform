a749472392ec3d582f493e4b908e7d4509417727
Yuri Miguel <yuri.md13@gmail.com>
2023-12-19 17:03:32 -0300

    criação da instância e declaração de credenciais

--- main.tf
+++ main.tf
@@ -7,3 +7,10 @@ resource "aws_instance" "web_server" {
     instance_type = "t2.micro" #Tipo da máquina, neste caso uma de baixo custo/performance (também pode ser modificada de acordo com a necessidade)
 }
 
+tags = {
+    Name = "web-server-instance"
+}
+
+#os campos abaixo são de uso pessoal, portanto deve-se colocar as credenciais pessoais  do usuário
+key_name       = "sua_chave_ssh"    #Substituir pelo nome da chave SSH
+security_group = ["Default"]        #Substituir pelo ID do grupo de segurança desejado
